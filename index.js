bbb7bb04de479a8137f6df5af0a66e90dbeeb8d0
Ronel028 <floridaronel15@gmail.com>
2023-01-28 22:18:55 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+require("dotenv").config();
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+app.use(cors());
+
+app.get("/api", async (request, response) => {
+  const job = request.query.job;
+  const location = "london";
+  const jobList = await axios.get(
+    `https://serpapi.com/search.json?engine=google_jobs&q=${job}&location=${location}&hl=en&api_key=${process.env.API_KEY}`
+  );
+  response.json(jobList.data);
+});
+
+app.listen(PORT, () => {
+  console.log("server running");
+});
